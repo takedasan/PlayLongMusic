a021eeced9c5cd51808391a35cd745305ddc6eda
takeda <takedasan593@gmaill.com>
2020-05-19 23:53:50 +0900

    Initial Commit

---
+++ PlayLongMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayLongMusic
+//
+//  Created by 竹田晴樹 on 2020/05/19.
+//  Copyright © 2020 takeda_san. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PlayLongMusicTests/PlayLongMusicTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlayLongMusicTests.swift
+//  PlayLongMusicTests
+//
+//  Created by 竹田晴樹 on 2020/05/19.
+//  Copyright © 2020 takeda_san. All rights reserved.
+//
+
+import XCTest
+@testable import PlayLongMusic
+
+class PlayLongMusicTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
