5b492a6dc91a5fb88364f1ef5b89689080947d57
takeda <takedasan593@gmaill.com>
2020-06-05 00:14:26 +0900

    ファイルアップロードの追加

--- PlayLongMusic/ViewController.swift
+++ PlayLongMusic/ViewController.swift
@@ -1,10 +1,12 @@
 import UIKit
 import AVFoundation
+import Telegraph
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
     // MARK: Properties
     var audioPlayer: AVAudioPlayer!
+    var server: Server!
     
     // MARK: UI Properties
     @IBOutlet weak var playButton: UIButton!
@@ -12,8 +14,20 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        server = Server()
+        try! server.start(port: 9000)
+        server.route(.GET, "", handle)
+        server.route(.POST, "", handlePost)
+    }
+
+    func handlePost(request: HTTPRequest) -> HTTPResponse {
+        return HTTPResponse(content: request.body.base64EncodedString())
     }
     
+    func handle(request: HTTPRequest) -> HTTPResponse {
+        return HTTPResponse(content: #"<b>desuto</b> <form action="/" method="post"><input type="file" accept="image/png, image/jpeg"></input><input type="submit" value="Upload Image" name="submit"></form>"#)
+    }
+
     // MARK: Actions
     @IBAction func pushPlayButton(_ sender: UIButton) {
         self.togglePlayButton()
